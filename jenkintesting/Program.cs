ca063310a68be353fb34d6c6e00e07cd2097f575
Aizaz A <aizaz.a@outlook.com>
2018-05-22 12:04:37 +0500

    Error Removed
    Error Removed

--- jenkintesting/Program.cs
+++ jenkintesting/Program.cs
@@ -10,7 +10,7 @@ namespace jenkintesting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("First Change")
+            Console.WriteLine("First Change");
                 Console.WriteLine("First Change");
 
 
