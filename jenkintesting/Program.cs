c928ded8ff80ed67d6d60d07b8be30f2cde44180
Aizaz A <aizaz.a@outlook.com>
2018-05-22 10:48:16 +0500

    First Change
    First Change

--- jenkintesting/Program.cs
+++ jenkintesting/Program.cs
@@ -10,7 +10,7 @@ namespace jenkintesting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Jenkin");
+            Console.WriteLine("First Change");
              
         }
     }
